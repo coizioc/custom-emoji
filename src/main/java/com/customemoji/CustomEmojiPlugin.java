dc61bd4a52c5a4d991b3210bc4146f7a65f1b0da
coizioc <36041795+coizioc@users.noreply.github.com>
2020-05-16 17:05:07 -0400

    Set up gradle config.

--- src/main/java/com/customemoji/CustomEmojiPlugin.java
+++ src/main/java/com/customemoji/CustomEmojiPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.customemoji;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,26 +14,26 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Custom Emoji"
 )
-public class ExamplePlugin extends Plugin
+public class CustomEmojiPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private CustomEmojiConfig config;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("Custom Emoji started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Custom Emoji stopped!");
 	}
 
 	@Subscribe
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	CustomEmojiConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(CustomEmojiConfig.class);
 	}
 }
